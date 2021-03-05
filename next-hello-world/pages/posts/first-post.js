c4829bdbaf385886b85aac4ff8af888c7b53ddf6
Pranoto Budi <pranotobudi.s@gmail.com>
2021-03-05 11:04:33 +0700

    add profile and round image

--- next-hello-world/pages/posts/first-post.js
+++ next-hello-world/pages/posts/first-post.js
@@ -9,7 +9,7 @@ const Profile = () => (
     src = '/images/profile.jpg'
     height={144} // Desired size with correct aspect ratio
     width={144} // Desired size with correct aspect ratio
-    alt="Your Name"
+    alt="Pranoto Budi"
   />
 )
 
@@ -21,12 +21,6 @@ export default function FirstPost() {
         <title>Create Next App</title>
 
         <h1 className={styles.title}>First Post</h1>
-        <h2 className={styles.title}>
-          <Link href="/">
-            <a>Back to home</a>
-          </Link>
-        </h2>
-        <Profile />
         </Layout>
       </>  
     ); 
