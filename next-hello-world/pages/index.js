bdf6912a5de3919d8e414a9d9bac850fae66a064
Pranoto Budi <pranotobudi.s@gmail.com>
2021-03-05 15:15:43 +0700

    fetching from file and prerender

--- next-hello-world/pages/index.js
+++ next-hello-world/pages/index.js
@@ -3,21 +3,46 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm a backend web developer, passionate at Go, Python, Elixir (Phoenix), PHP (Laravel) and of course [a must] Javascript (React.js Next.js, Vue.js Nuxt.js, Node.js Express.js Nest.js), </p>
+        <p>Hi, I'm a backend web developer, passionate at Go, Python, PHP (Laravel, wordpress) and Javascript (React.js Next.js, Vue.js Nuxt.js, Node.js Express.js Nest.js), </p>
         <p>
           (Programming is a long journey - it's a marathon not a sprint{' '}
           <a href="https://nextjs.org/learn">
             another Next.js tutorial check out here
           </a>.)
         </p>
+      </section>
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
         <div className={styles.backToHome}>
           <Link href="/posts/first-post">
